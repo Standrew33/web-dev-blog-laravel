fdb7e9f60c47adb5731120d25862b6e42ce9caa1
Andrei Zhidkov <zidkov@orangelabel.com>
2024-02-05 23:45:23 +0100

    Models & Controllers complete

--- app/routes/web.php
+++ app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Task;
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
@@ -51,15 +52,21 @@ Route::get('hello5', function () {
     return view('hello', compact('tasks'));
 });
 
-Route::get('/tasks', function () {
-    //$tasks = DB::table('tasks')->get();
-    //$tasks = Task::all();
-    $tasks = Task::incomplete();
-    return view('tasks.index', compact('tasks'));
-});
+//Route::get('/tasks', function () {
+//    //$tasks = DB::table('tasks')->get();
+//    //$tasks = Task::all();
+//    $tasks = Task::incomplete();
+//    return view('tasks.index', compact('tasks'));
+//});
+//
+//Route::get('/tasks/{task}', function ($id) {
+//    //$task = DB::table('tasks')->find($id);
+//    $task = Task::find($id);
+//    return view('tasks.show', compact('task'));
+//});
 
-Route::get('/tasks/{task}', function ($id) {
-    //$task = DB::table('tasks')->find($id);
-    $task = Task::find($id);
-    return view('tasks.show', compact('task'));
-});
+//Route::get('/tasks', 'TasksController@index');
+//Route::get('/tasks/{task}', 'TasksController@show');
+
+Route::get('tasks',[TasksController::class, 'index']);
+Route::get('tasks/{task}',[TasksController::class, 'show']);
